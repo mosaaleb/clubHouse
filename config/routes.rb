1a88dfccc9524806510932260e9a6836cf5d1e06
Saheed <sdrfngg640@gmail.com>
2019-05-13 16:04:59 +0100

    Generate posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'posts/new'
+  get 'posts/create'
+  get 'posts/index'
   resources :homes, only: [:index]
   resource :session, only: %i[new create destroy]
   root 'homes#index'
